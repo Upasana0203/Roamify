aff2fd0b2801865e2af7b2a65524f5e9949d4660
Upasana Bhandarkar <upasanabhandarkar100@gmail.com>
2025-04-28 23:41:21 +0530

    Add Project Files

---
+++ routes/user.js
@@ -0,0 +1,44 @@
+const express=require("express");
+const router=express.Router();
+const User=require("../models/user.js");
+const wrapAsync=require("../utils/wrapAsync.js");
+const passport=require("passport");
+const{saveRedirectUrl}=require("../middleware.js");
+const userController=require("../controllers/users.js");
+
+router
+.route("/signup")
+.get(userController.renderSignupForm )
+.post(wrapAsync(userController.signup));
+
+
+router
+.route("/login")
+.get(userController.renderLoginForm)
+.post(saveRedirectUrl,
+    passport.authenticate("local", { failureRedirect: '/login', failureFlash:true }),
+    userController.login);
+
+//render signup form
+// router.get("/signup",userController.renderSignupForm );
+
+//signup
+// router.post("/signup",wrapAsync(userController.signup));
+
+// render login form
+// router.get("/login",userController.renderLoginForm);
+
+//login
+// router.post("/login",saveRedirectUrl,
+// passport.authenticate("local", { failureRedirect: '/login', failureFlash:true }),
+// userController.login);
+
+
+
+//logout
+router.get("/logout",userController.logout);
+
+
+
+
+module.exports=router;
